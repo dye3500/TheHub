a3604ae98d6d249381f046b4c3500a47fc8ab6f6
Danny <dye8889@gmail.com>
2023-02-10 17:51:52 -0800

    Lab03-Danny

--- User.java
+++ User.java
@@ -1,2 +1,33 @@
 public class User {
+    //new Username and Password (going to later do a forget username/password and send to email if time)
+    public class newUsername {
+        private String newUsername;
+
+        public newUsername(String username) {
+            this.newUsername = username;
+        }
+
+        public String getNewUsername() {
+            return username;
+        }
+
+        public void setNewUsername(String username) {
+            this.newUsername = username;
+        }
+    }
+
+    public class newPassword {
+        private String newPassword;
+
+        public newPassword(String password) {
+            this.newPassword = password;
+        }
+
+        public String getNewpassword() {
+            return password;
+        }
+
+        public void setNewPassword(String password) {
+            this.newPassword = password;
+        }
 }
